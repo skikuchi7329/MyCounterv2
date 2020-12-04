7981c6b3e435a7990fb789f086ce326a0bca2c3a
Shohei Kikuchi <killchance2256@gmail.com>
2020-12-04 23:01:58 +0900

    add counter

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const counter = () => {
+  let count = 0;
+  const counterDOM = document.querySelector("#counter");
+  const incrementBtn = document.querySelector("#increment");
+
+  const increment = () => {
+    count++
+  }
+  const render = () => {
+    counterDOM.textContent = count;
+  }
+  return {
+    init() {
+      counterDOM.addEventListener('click', () => {
+        increment();
+        render();
+      })
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const counter = Counter();
+  counter.init();
+})
